058b930e42401281ad0ef791844d1836d1016eb1
crybabyh3h3 <timothynico171103@gmail.com>
2024-07-31 12:28:57 +0700

    init

--- app/Http/Controllers/PendaftaranController.php
+++ app/Http/Controllers/PendaftaranController.php
@@ -4,8 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Pendaftaran;
 
 class PendaftaranController extends Controller
 {
-    //
+    public function submit(Request $request)
+    {
+        // Validasi input
+        $request->validate([
+            'user_id' => 'required|numeric',
+            'ukm_id' => 'required|numeric',
+        ]);
+
+        // Simpan data ke database
+        Pendaftaran::create([
+            'user_id' => $request->user_id,
+            'ukm_id' => $request->ukm_id,
+            'status_diterima' => false,
+            'status_pembayaran' => false,
+        ]);
+
+        return redirect()->back()->with('success', 'Pendaftaran berhasil!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PendaftaranController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('user/pendaftaran', function () {
+    return view('/user/pendaftaran');
+});
+Route::post('user/pendaftaran', [PendaftaranController::class, 'submit']);
