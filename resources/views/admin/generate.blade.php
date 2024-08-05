e0a3ba56455692b5c7e5f0f4c4aa2be6967a9e4f
warrnn <bryanwarren86@gmail.com>
2024-08-05 22:27:58 +0700

    Validate page

--- resources/views/admin/generate.blade.php
+++ resources/views/admin/generate.blade.php
@@ -3,7 +3,7 @@
 <div class="h-full flex justify-center px-4">
     <div class="h-auto w-auto mx-auto mt-56 mb-auto lg:my-auto bg-gradient-to-tl from-indigo-300 to-blue-800 p-6 rounded-xl drop-shadow-lg z-10">
         <p class="text-3xl text-white font-bold text-center mb-2">OpenHouse Game Words Generator</p>
-        <p class="text-white text-center mb-6">Input the NRP of the students to generate their collectible words.</p>
+        <p class="text-white text-center mb-6">Input participants NRP to generate their collectible words.</p>
         <form action="" method="" class="flex flex-col gap-4">
             <input type="text" name="nrp" placeholder="NRP" class="h-12 p-2 border-2 rounded">
             <input type="submit" class="font-semibold mb-2 h-12 border-2 bg-white hover:bg-green-400 transition rounded-lg">
--- resources/views/admin/validate.blade.php
+++ resources/views/admin/validate.blade.php
@@ -1,4 +1,61 @@
 @extends('admin.layouts.layout')
 @section('content')
-
+<div class="h-full justify-center px-4">
+    <h1 class="text-3xl font-bold py-4">List Pendaftar & Validasi</h1>
+    <div class="overflow-auto rounded-lg bg-white drop-shadow-lg">
+        <table class="w-full">
+            <thead class="bg-gray-200 border-b-2 border-gray-200">
+                <tr class="text-left text-nowrap">
+                    <th class="p-3 text-md font-bold tracking-wide border-e-2 border-gray-200">NRP</th>
+                    <th class="p-3 text-md font-bold tracking-wide border-e-2 border-gray-200">Nama</th>
+                    <th class="p-3 text-md font-bold tracking-wide border-e-2 border-gray-200">UKM</th>
+                    <th class="p-3 text-md font-bold tracking-wide border-e-2 border-gray-200">File Seleksi</th>
+                    <th class="p-3 text-md font-bold tracking-wide border-e-2 border-gray-200">Status Seleksi</th>
+                    <th class="p-3 text-md font-bold tracking-wide border-e-2 border-gray-200">Bukti Transfer</th>
+                    <th class="p-3 text-md font-bold tracking-wide border-e-2 border-gray-200">Status Transfer</th>
+                    <th class="p-3 text-md font-bold tracking-wide"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="text-nowrap hover:bg-amber-100 transition">
+                    <td class="p-3 border-e-2 border-gray-200">C14220004</td>
+                    <td class="p-3 border-e-2 border-gray-200">Bryan Mogens Warren</td>
+                    <td class="p-3 border-e-2 border-gray-200">Basket</td>
+                    <td class="p-3 border-e-2 border-gray-200 text-center">
+                        <a href="">
+                            <button class="p-1.5 text-sm bg-sky-500 hover:bg-sky-600 transition text-white text-nowrap rounded">
+                                Open Drive
+                            </button>
+                        </a>
+                    </td>
+                    <td class="p-3 border-e-2 border-gray-200">
+                        <span class="font-bold text-green-500 rounded">Yes</span>
+                    </td>
+                    <td class="p-3 border-e-2 border-gray-200 text-center">
+                        <a href="">
+                            <button class="p-1.5 text-sm bg-sky-500 hover:bg-sky-600 transition text-white text-nowrap rounded">
+                                Open File
+                            </button>
+                        </a>
+                    </td>
+                    <td class="p-3 border-e-2 border-gray-200">
+                        <span class="font-bold text-red-500 rounded">No</span>
+                    </td>
+                    <td class="p-3 border-s-2 text-center text-nowrap">
+                        <a href="">
+                            <button class="p-1.5 text-sm bg-green-500 hover:bg-green-600 transition text-white text-nowrap rounded">
+                                Validate
+                            </button>
+                        </a>
+                        <a href="">
+                            <button class="p-1.5 text-sm bg-red-500 hover:bg-red-600 transition text-white text-nowrap rounded">
+                                Reject
+                            </button>
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
