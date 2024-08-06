62fb4a3b789abc9739fff12b638d779b619d4651
William0389 <c14220049@john.petra.ac.id>
2024-08-06 19:07:28 +0700

    fix : responsive homepage (again :')

--- resources/views/user/homepage.blade.php
+++ resources/views/user/homepage.blade.php
@@ -18,45 +18,45 @@
         class="absolute hidden lg:block top-[450vh] sm:left-[-10vw] left-[-15vw] rotate-[30deg] sm:w-1/3 w-1/2">
 
     {{-- Maskot --}}
-    {{-- <img src="{{ asset('assets/CORALIS POSE 1.png') }}" alt="Coralis"
-        class="absolute lg:right-[10vw] right-[-25vw] top-[555vh] lg:w-1/5 w-1/2">
+    <img src="{{ asset('assets/CORALIS POSE 1.png') }}" alt="Coralis"
+        class="maskot absolute lg:right-[-5vw] right-[-25vw] sm:top-[2600px] md:top-[3000px] lg:top-[3550px] xl:top-[4700px] top-[1900px] lg:w-1/3 w-1/2">
     <img src="{{ asset('assets/VERDARA POSE 1.png') }}" alt="Verdara"
-        class="absolute lg:left-[10vw] left-[-18vw] top-[555vh] lg:w-1/5 w-1/2"> --}}
+        class="maskot absolute lg:left-[-5vw] left-[-18vw] sm:top-[2600px] md:top-[3000px] lg:top-[3550px] xl:top-[4700px] top-[1900px] lg:w-1/3 w-1/2">
     <section class="video w-full xl:h-[120vh] z-50">
         <video src="{{ asset('assets/Video OH.mp4') }}" autoplay muted loop preload="auto"></video>
     </section>
+    <div class="flex justify-center mt-10">
+        <h1 data-aos="fade-down" data-aos-once="true"
+            class="text-7xl font-bold bg-gradient-to-r from-[#DEC47C] via-[#F7EECF] to-[#DEC47C] inline-block text-transparent bg-clip-text">
+            TIMELINE</h1>
+    </div>
     <section class="timeline" id="timeline">
-        <div class="flex justify-center">
-            <h1 data-aos="fade-down" data-aos-once="true"
-                class="text-7xl font-bold bg-gradient-to-r from-[#DEC47C] via-[#F7EECF] to-[#DEC47C] inline-block text-transparent bg-clip-text">
-                TIMELINE</h1>
-        </div>
         <div class="img-container flex items-end w-[300vw] overflow-x-scroll mx-auto no-scrollbar">
             <div class="pulau1 w-[100vw] flex justify-center">
-                <img src="{{ asset('assets/SS.png') }}" class="w-[200px]" alt="">
+                <img src="{{ asset('assets/SS.png') }}" class="sm:w-[200px] w-[150px]" alt="">
             </div>
             <div class="pulau2 w-[100vw] flex justify-center">
-                <img src="{{ asset('assets/Floating Rocks.png') }}" class="w-[200px]" alt="">
+                <img src="{{ asset('assets/Floating Rocks.png') }}" class="sm:w-[200px] w-[150px]" alt="">
             </div>
             <div class="pulau3 w-[100vw] flex justify-center">
-                <img src="{{ asset('assets/EO.png') }}" class="w-[200px]" alt="">
+                <img src="{{ asset('assets/EO.png') }}" class="sm:w-[200px] w-[150px]" alt="">
             </div>
         </div>
         <div class="timeline mt-10 relative flex justify-center">
-            <img src="{{ asset('assets/line.png') }}" class="absolute w-10/12 top-1 sm:top-0" alt="">
-            <img src="{{ asset('assets/point_passive.png') }}" class="absolute md:w-[2%] w-[5%] md:left-52 left-16"
+            <img src="{{ asset('assets/line.png') }}" class="absolute xl:w-10/12 w-full md:top-1 sm:top-3 xl:top-0 top-2" alt="">
+            <img src="{{ asset('assets/point_passive.png') }}" class="absolute md:w-[4%] xl:w-[2%] w-[6%] xl:left-52 left-16"
                 alt="">
-            <img src="{{ asset('assets/point_passive.png') }}" class="absolute md:w-[2%] w-[5%]" alt="">
-            <img src="{{ asset('assets/point_passive.png') }}" class="absolute md:w-[2%] w-[5%] md:right-52 right-16"
+            <img src="{{ asset('assets/point_passive.png') }}" class="absolute md:w-[4%] xl:w-[2%] w-[6%]" alt="">
+            <img src="{{ asset('assets/point_passive.png') }}" class="absolute md:w-[4%] xl:w-[2%] w-[6%] xl:right-52 right-16"
                 alt="">
         </div>
         <div class="active-timeline relative flex justify-center">
-            <img src="{{ asset('assets/point_active.png') }}" class="active1 absolute md:w-[2%] w-[5%] md:left-52 left-16"
+            <img src="{{ asset('assets/point_active.png') }}" class="active1 absolute md:w-[4%] xl:w-[2%] w-[6%] xl:left-52 left-16"
                 alt="">
-            <img src="{{ asset('assets/point_active.png') }}" class="active2 absolute md:w-[2%] w-[5%] opacity-0"
+            <img src="{{ asset('assets/point_active.png') }}" class="active2 absolute md:w-[4%] xl:w-[2%] w-[6%] opacity-0"
                 alt="">
             <img src="{{ asset('assets/point_active.png') }}"
-                class="active3 absolute md:w-[2%] w-[5%] md:right-52 right-16 opacity-0" alt="">
+                class="active3 absolute md:w-[4%] xl:w-[2%] w-[6%] xl:right-52 right-16 opacity-0" alt="">
         </div>
         <div class="text-timeline mt-10 md:space-x-24 font-bold flex justify-around">
             <h1
@@ -69,18 +69,82 @@
                 class="text-xl font-bold bg-gradient-to-r from-[#DEC47C] via-[#F7EECF] to-[#DEC47C] inline-block text-transparent bg-clip-text">
                 30 Agustus</h1>
         </div>
-        <div class="content mt-10 p-5 w-1/2 rounded-2xl text-center bg-white mx-auto">
-            <h1 class="content1 font-bold text-5xl hidden">Content1</h1>
-            <h1 class="content2 font-bold text-5xl hidden">Content2</h1>
-            <h1 class="content3 font-bold text-5xl hidden">Content3</h1>
+        <div
+            class="content mt-10 p-5 lg:w-1/2 h-[300px] overflow-y-auto overflow-x-hidden w-10/12 rounded-2xl text-center bg-[#3586ff3d] mx-auto text-black shadow-black shadow-lg backdrop-blur-md">
+            <div class="content1 hidden">
+                <h1 class="font-bold text-3xl">Performance of the Week</h1>
+                <h5 class="font-bold text-xl">16 Agustus 2024</h5>
+
+                <ul class="list-disc text-start ms-3 mt-5">
+                    <li>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, error?</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, molestias.</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, delectus.</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, unde.</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, unde.</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, unde.</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, unde.</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="content2 hidden">
+                <h1 class="font-bold text-3xl">Performance of the Week</h1>
+                <h5 class="font-bold text-xl">23 Agustus 2024</h5>
+
+                <ul class="list-disc text-start ms-3">
+                    <li>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus, reprehenderit.</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, suscipit?</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, ea.</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, unde.</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="content3 hidden">
+                <h1 class="font-bold text-3xl">Performance of the Week</h1>
+                <h5 class="font-bold text-xl">30 Agustus 2024</h5>
+
+                <ul class="list-disc text-start ms-3">
+                    <li>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, odit!</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, totam.</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, sint.</p>
+                    </li>
+                    <li>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, minima.</p>
+                    </li>
+                </ul>
+            </div>
         </div>
     </section>
 
     <section class="game-ukm flex justify-center space-x-5 mb-10" id="game-ukm">
         <a href="#">
             <div
-                class="border rounded-xl bg-[#074174b3] backdrop-blur-xl shadow-white shadow-lg md:min-w-[200px] p-5 space-y-5 flex flex-col justify-center text-center transition-shadow hover:ease-in-out duration-200 hover:shadow-white hover:shadow-xl">
-                <span class="text-center mx-auto sm:h-[120px] sm:w-[150px] h-[60px] w-[75px]">
+                class="border rounded-xl bg-[#074174b3] backdrop-blur-sm shadow-white shadow-lg md:min-w-[200px] p-5 space-y-5 flex flex-col justify-center text-center transition-shadow hover:ease-in-out duration-200 hover:shadow-white hover:shadow-xl">
+                <span class="text-center mx-auto sm:h-[120px] sm:w-[150px] h-[100px] w-[125px]">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
                         <path fill="#ffffff"
                             d="M274.9 34.3c-28.1-28.1-73.7-28.1-101.8 0L34.3 173.1c-28.1 28.1-28.1 73.7 0 101.8L173.1 413.7c28.1 28.1 73.7 28.1 101.8 0L413.7 274.9c28.1-28.1 28.1-73.7 0-101.8L274.9 34.3zM200 224a24 24 0 1 1 48 0 24 24 0 1 1 -48 0zM96 200a24 24 0 1 1 0 48 24 24 0 1 1 0-48zM224 376a24 24 0 1 1 0-48 24 24 0 1 1 0 48zM352 200a24 24 0 1 1 0 48 24 24 0 1 1 0-48zM224 120a24 24 0 1 1 0-48 24 24 0 1 1 0 48zm96 328c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-192c0-35.3-28.7-64-64-64l-114.3 0c11.6 36 3.1 77-25.4 105.5L320 413.8l0 34.2zM480 328a24 24 0 1 1 0 48 24 24 0 1 1 0-48z" />
@@ -93,8 +157,8 @@
         </a>
         <a href="#">
             <div
-                class="border rounded-xl bg-[#074174b3] backdrop-blur-xl shadow-white shadow-lg md:min-w-[200px] p-5 space-y-5 flex flex-col justify-center text-center transition-shadow hover:ease-in-out duration-200 hover:shadow-white hover:shadow-xl">
-                <span class="text-center mx-auto sm:h-[120px] sm:w-[135px] h-[60px] w-[75px]">
+                class="border rounded-xl bg-[#074174b3] backdrop-blur-sm shadow-white shadow-lg md:min-w-[200px] p-5 space-y-5 flex flex-col justify-center text-center transition-shadow hover:ease-in-out duration-200 hover:shadow-white hover:shadow-xl">
+                <span class="text-center mx-auto sm:h-[120px] sm:w-[135px] h-[100px] w-[125px]">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                         <path fill="#ffffff"
                             d="M0 80l0 48c0 17.7 14.3 32 32 32l16 0 48 0 0-80c0-26.5-21.5-48-48-48S0 53.5 0 80zM112 32c10 13.4 16 30 16 48l0 304c0 35.3 28.7 64 64 64s64-28.7 64-64l0-5.3c0-32.4 26.3-58.7 58.7-58.7L480 320l0-192c0-53-43-96-96-96L112 32zM464 480c61.9 0 112-50.1 112-112c0-8.8-7.2-16-16-16l-245.3 0c-14.7 0-26.7 11.9-26.7 26.7l0 5.3c0 53-43 96-96 96l176 0 96 0z" />
