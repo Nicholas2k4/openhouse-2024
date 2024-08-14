87b11f49357622841bdb61032affe32e8c27c528
Jeffry Hermawan <jeffryhermawantm@gmail.com>
2024-08-15 00:54:21 +0700

    fix: input admin booth bug

--- resources/views/admin/includes/navigation.blade.php
+++ resources/views/admin/includes/navigation.blade.php
@@ -80,10 +80,7 @@
         <!-- List Navigation -->
         <div class="mt-6">
             <ul>
-                @if (session('ukm_slug') == null &&
-                        (session('division_slug') == 'sekkonkes' ||
-                            session('division_slug') == 'it' ||
-                            session('division_slug') == 'bph'))
+                @if (session('division_slug') == 'sekkonkes' || session('division_slug') == 'it' || session('division_slug') == 'bph'|| session('field') == 'ketua')
                     <a href="{{ route('admin.booth-admin') }}" class="text-md">
                         <li class="flex flex-row items-center p-3 my-2 hover:bg-gray-200 rounded-lg transition">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="me-2"
@@ -173,10 +170,7 @@
 <!-- Navbar Content -->
 <div id="smallNav" class="bg-white border-b-2 absolute w-full hidden z-[999]">
     <ul class="mx-2">
-        @if (session('ukm_slug') == null &&
-                (session('division_slug') == 'sekkonkes' ||
-                    session('division_slug') == 'it' ||
-                    session('division_slug') == 'bph'))
+        @if (session('division_slug') == 'sekkonkes' || session('division_slug') == 'it' || session('division_slug') == 'bph')
             <a href="{{ route('admin.booth-admin') }}" class="text-md">
                 <li class="flex flex-row items-center p-3 my-2 hover:bg-gray-200 rounded-lg transition">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="me-2"
@@ -222,8 +216,8 @@
         </li>
         <li class="p-4 my-2 hover:bg-rose-500 rounded-lg transition">
             <a href={{ route('admin.logout') }} class="flex flex-row text-md items-center justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="me-2"
-                    viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="me-2" viewBox="0 0 16 16">
                     <path fill-rule="evenodd"
                         d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
                     <path fill-rule="evenodd"
