4c0c24a87ac2dfdaf19cad5dd59eac62dd6f5830
Jeffry Hermawan <jeffryhermawantm@gmail.com>
2024-08-14 19:17:17 +0700

    try: remove xml 2

--- resources/views/user/components/loader.blade.php
+++ resources/views/user/components/loader.blade.php
@@ -168,7 +168,6 @@
     </div>
     <div class="loader overflow-hidden w-screen h-screen flex justify-center items-center flex-col fixed"
         id="loader2">
-        <?xml version="1.0" encoding="UTF-8"?>
         <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="3840px" height="2160px"
             class="fixed sm:scale-[0.16] max-sm:scale-[0.09]"
