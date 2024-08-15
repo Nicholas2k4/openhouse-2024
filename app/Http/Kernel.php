a2a6ac4a63483610d696ba691ad814824e959f5e
Jeffry Hermawan <jeffryhermawantm@gmail.com>
2024-08-15 17:04:46 +0700

    fix: middleware in kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'isLogin' => \App\Http\Middleware\isLogin::class,
         'user' => \App\Http\Middleware\UserMiddleware::class,
+        'isClosed' => \App\Http\Middleware\ClosedRegistration::class,
     ];
 }
