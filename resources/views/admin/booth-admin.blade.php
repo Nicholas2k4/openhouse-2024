be66cc1215fe7065bdf644487c7159343a7d2157
Jeffry Hermawan <jeffryhermawantm@gmail.com>
2024-08-15 17:33:28 +0700

    d

--- resources/views/admin/booth-admin.blade.php
+++ resources/views/admin/booth-admin.blade.php
@@ -13,7 +13,7 @@
         <div class="h-full flex justify-center px-4">
             <div
                 class="h-auto w-auto mx-auto mt-56 mb-auto lg:my-auto bg-gradient-to-tl from-indigo-300 to-blue-800 p-6 rounded-xl drop-shadow-lg z-10">
-                <p class="text-3xl text-white font-bold text-center mb-2">Input Data Admin Penjaga Booth UKM</p>
+                <p class="text-3xl text-white font-bold text-center mb-4">Input Data Admin Penjaga Booth UKM</p>
                 <form id="" action="{{ route('admin.store-admin-booth') }}" method="POST" class="flex flex-col gap-4">
                     @csrf
                     <input type="text" name="nrp" placeholder="NRP" class="h-12 p-2 border-2 rounded" required>
