ca39bd59cafe2b996f1da0a88b49fc6acd0c05b2
Jeffry Hermawan <jeffryhermawantm@gmail.com>
2024-08-15 08:17:24 +0700

    fix: game: auth to session

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -9,17 +9,20 @@ class GameController extends Controller
 {
     public function index()
     {
-        if (auth()->user()) {
-            $nrp = auth()->user()->nrp;
+        // if (auth()->user()) {
+        //     $nrp = auth()->user()->nrp;
+        // } else {
+        //     $nrp = 0;
+        // }
+
+        if (session()->has('nrp')) {
+            $nrp = session('nrp');
         } else {
             $nrp = 0;
         }
 
-
         // DAY 1 - Be ready to welcome the new dawn!
         $day1Sentence = "Be ready to welcome the new dawn!";
-
-
 
         // DAY 2 - The coming of brighter tomorrow
         // $day2Sentence = "The coming of brighter tomorrow"
@@ -28,7 +31,7 @@ class GameController extends Controller
         // $day3Sentence = "For he is risen so we are destined"
 
         $letters = DB::table('detail_games')->where('nrp', $nrp)->pluck('letter', 'letter_index');
-        $letters = $letters ->toArray();
+        $letters = $letters->toArray();
 
         $day1SentenceNoSpaces = str_replace(' ', '', $day1Sentence);
         $userSentence = '';
