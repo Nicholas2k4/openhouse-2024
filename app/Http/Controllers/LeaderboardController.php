a2782c05a5f1e91e04a811a3ae86a5590d15339a
warrnn <bryanwarren86@gmail.com>
2024-08-18 23:36:32 +0700

    Fixing fetch liderbort LAGI

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -9,22 +9,34 @@ use Illuminate\Support\Facades\DB;
 
 class LeaderboardController extends Controller
 {
-    public function fetchLeaderboard(Request $request) {
+    public function fetchLeaderboard(Request $request)
+    {
         $output = '';
         if ($request->ajax()) {
-            $data = DetailGame::select('nrp', DB::raw('COUNT(*) as score'))->groupBy('nrp')->orderBy('score', 'desc')->get();
+            // Fetching leaderboard data
+            $data = DetailGame::select('nrp', DB::raw('COUNT(*) as score'))
+                ->groupBy('nrp')
+                ->orderBy('score', 'desc')
+                ->get();
+
+            // Fetch User in a single query
+            $userDetails = User::whereIn('nrp', $data->pluck('nrp'))->get()->keyBy('nrp');
 
             foreach ($data as $row) {
-                $output .= '
-                <tr class="text-nowrap text-md hover:bg-amber-100 transition">
-                    <td class="p-3 border-e-2 border-gray-200 flex flex-col">
-                        <span id="thisRowNrp" class="font-semibold">' . $row->nrp . '</span>
-                        <span>' . User::where('nrp', $row->nrp)->first()->name  . '</span>
-                    </td>
-                    <td class="p-3 border-e-2 border-gray-200">' . User::where('nrp', $row->nrp)->first()->line_id . '</td>
-                    <td class="p-3 border-e-2 border-gray-200">' . User::where('nrp', $row->nrp)->first()->phone . '</td>
-                    <td class="p-3 border-e-2 border-gray-200">' . $row->score . '</td>
-                </tr>';
+                $user = $userDetails->get($row->nrp);
+
+                if ($user) {
+                    $output .= '
+                    <tr class="text-nowrap text-md hover:bg-amber-100 transition">
+                        <td class="p-3 border-e-2 border-gray-200 flex flex-col">
+                            <span id="thisRowNrp" class="font-semibold">' . $row->nrp . '</span>
+                            <span>' . $user->name . '</span>
+                        </td>
+                        <td class="p-3 border-e-2 border-gray-200">' . $user->line_id . '</td>
+                        <td class="p-3 border-e-2 border-gray-200">' . $user->phone . '</td>
+                        <td class="p-3 border-e-2 border-gray-200">' . $row->score . '</td>
+                    </tr>';
+                }
             }
 
             return response()->json(['allRanks' => $output, 'dataCount' => $data->count()]);
